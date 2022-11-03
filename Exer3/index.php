c01e3636dbcb67469e837abddc3055767bc61860
Murilo-FernandesMello <murilofmello07@gmail.com>
2022-11-03 19:30:11 -0300

    Third Commit

---
+++ Exer3/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./front3.css">
+    <title>EXERCICIO 3</title>
+</head>
+<body>
+    <Form action="exer3.php" method="post">
+        <h1>Cálculo</h1>
+        <br><br>
+        <label for="a">Valor 1</label>
+        <input type="number_format(int)" id="a" name="a"/>
+        <br><br>
+        <label for="b">Valor 2</label>
+        <input type="number_format(int)" id="b" name="b"/>
+        <br><br>
+        <label for="c">Valor 3</label>
+        <input type="number_format(int)" id="c" name="c"/>
+        <br><br>
+        <input type="submit" value="Calcular">
+    </Form>
+    
+</body>
+</html>
