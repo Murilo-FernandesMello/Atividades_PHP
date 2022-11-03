9afe8c347f7c2431366a454e3c9a8c16410533d1
Murilo-FernandesMello <murilofmello07@gmail.com>
2022-11-03 20:10:44 -0300

    Fifth Commit

---
+++ Exer5/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./front5.css">
+    <title>EXERCICIO 3</title>
+</head>
+<body>
+    <Form action="exer5.php" method="post">
+        <h1>Cálculo</h1>
+        <br><br>
+        <label for="a">Primeiro Valor</label>
+        <input type="number_format(int)" id="a" name="a"/>
+        <br><br>
+        <label for="b">Segundo Valor</label>
+        <input type="number_format(int)" id="b" name="b"/>
+        <br><br>
+        <label for="c">Terceiro Valor</label>
+        <input type="number_format(int)" id="c" name="c"/>
+        <br><br>
+        <input type="submit" value="Calcular">
+    </Form>
+    
+</body>
+</html>
