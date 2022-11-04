d1d76ff4ddb683e53d3f6b0ac3312abb437b28f6
Murilo-FernandesMello <murilofmello07@gmail.com>
2022-11-03 22:26:32 -0300

    Eighth Commit

---
+++ Exer8/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./front8.css">
+    <title>EXERCICIO 3</title>
+</head>
+<body>
+    <Form action="exer8.php" method="get">
+        <h1>Cálculo</h1>
+        <br><br>
+        <label for="vel">Velocidade</label>
+        <input type="number_format(int)" id="vel" name="vel"/>
+        <br><br>
+        <label for="tem">Tempo</label>
+        <input type="number_format(int)" id="tem" name="tem"/>
+        <br><br>
+        <input type="submit" value="Calcular">
+    </Form>
+    
+</body>
+</html>
